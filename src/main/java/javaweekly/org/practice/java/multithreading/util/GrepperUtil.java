040b9d07fbef6c41a4cd95d545a352eb9f9d0936
magnusram05 <ramachandranrajagopalan05@gmail.com>
2019-05-29 09:41:48 +0530

    Reverting cast to lambda

--- src/main/java/javaweekly/org/practice/java/multithreading/util/GrepperUtil.java
+++ src/main/java/javaweekly/org/practice/java/multithreading/util/GrepperUtil.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,12 +15,17 @@ public class GrepperUtil {
 
     private static Grepper<String> grepper = FileSystemGrepper.getInstance();
 
-    public static List<Callable<List<String>>>  getCallableSearchTasks(Stream<Path> paths, String searchString){
+    public static List<Callable<List<String>>>  getCallableSearchTasks(Stream<Path> paths, final String searchString){
         return paths
                 .filter(Files::isRegularFile)
                 .map(Path::toAbsolutePath)
                 .map(Path::toString)
-                .map((filePathString) -> (Callable<List<String>>)grepper.grep(filePathString, searchString))
+                .map((filePathString) -> callableSearchTasks.apply(filePathString, searchString))
                 .collect(Collectors.toList());
     }
+
+    public static BiFunction<String, String, Callable<List<String>>> callableSearchTasks = (filePathString, searchString)-> {
+        final Callable<List<String>> listCallable = () -> grepper.grep(filePathString, searchString);
+        return listCallable;
+    };
 }
